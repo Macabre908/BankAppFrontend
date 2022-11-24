0935c436ee0bdfde779dbf460e9c265cdb208cd1
Abhishek Dubey <abhishekdubey789901@gmail.com>
2022-11-25 00:48:25 +0530

    Completed the Navbar Component

--- BankApp/src/App.jsx
+++ BankApp/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Billing, Business, CardDeals, Clients, CTA, Footer, Hero, Navbar, Stats, Testimonials } from './components';
 import styles from './styles';
 
 const App = () => {
@@ -6,26 +7,26 @@ const App = () => {
     <div className='bg-primary w-full overflow-hidden'>
       <div className={`${styles.paddingX} ${styles.flexCenter}`}> 
         <div className={`${styles.boxWidth}`}>
-          Navbar
+          <Navbar />
         </div>
       </div>
 
       <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Hero
+          <Hero />
         </div>
       </div>
 
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Stats
-          Business 
-          Billing
-          CardDeal
-          Testimonials
-          Clients 
-          CTA 
-          Footer
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeals />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
         </div>
       </div>
 
